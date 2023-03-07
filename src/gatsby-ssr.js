59f55a1c10382e953bb65cc25688b8d3a57aa7b4
addonion <addonion@gmail.com>
2023-03-07 13:41:16 +0530

    Upgrade

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -18,7 +18,7 @@ exports.onRenderBody = ({ setHeadComponents }, { carrotquestId, mobileDelay = 0,
   const scriptInnerHTML =
     `window.carrotquestId = "${carrotquestId}";` +
     `if (typeof carrotquest === "undefined") {  function Build(name, args) {    return function () {      window.carrotquestasync.push(name, arguments);    };  }  window.carrotquest = {};  window.carrotquestasync = [];  carrotquest.settings = {};  var m = [    "connect",    "track",    "identify",    "auth",    "open",    "onReady",    "addCallback",    "removeCallback",    "trackMessageInteraction",  ];  for (var i = 0; i < m.length; i++) carrotquest[m[i]] = Build(m[i]);}` +
-    `function initCQ() {  var s = document.createElement("script");  s.type = "text/javascript";  s.async = true;  s.src = "//cdn.carrotquest.io/api.min.js";  var x = document.getElementsByTagName("head")[0];  x.appendChild(s);}` +
+    `function initCQ() {  var s = document.createElement("script");  s.type = "text/javascript";  s.async = true;  s.src = "//cdn.carrotquest.io/api.min.isolate.js";  var x = document.getElementsByTagName("head")[0];  x.appendChild(s);}` +
     delay;
 
   return setHeadComponents([<script key="gatsby-plugin-carrotquestquest" dangerouslySetInnerHTML={{ __html: scriptInnerHTML }} />]);
